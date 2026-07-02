fd4e1335064e0005b38b6cd73c9d6a0af4ae36bd
YuShiTing <shiy03313@gmail.com>
2026-07-03 01:05:49 +0800

    Update: 2026-07-03 01:05:48

--- images.js
+++ images.js
@@ -1,2 +1,4 @@
-const imgList = ["image\\20260703\\TWII_20260703.png"];
+const imgList = ["image/20260703/TWII_20260703.png"];
 const reportDate = '20260703';
+const indexImages = ["image/20260703/TWII_20260703.png"];
+const indexData = {"加權指數": {"symbol": "^TWII", "date": "2026-07-01", "close": 47018.99, "change": 893.08, "pct": 1.94}};
