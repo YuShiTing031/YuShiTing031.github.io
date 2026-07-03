2bf0759c4d3f75238a4795fae049091c7793ff34
YuShiTing <shiy03313@gmail.com>
2026-07-03 20:18:50 +0800

    Update: 2026-07-03 20:18:50

--- images.js
+++ images.js
@@ -1,4 +1,4 @@
-const imgList = [];
+const imgList = ["image/20260703/report_capital_STRONG_2026-07-03.png", "image/20260703/report_capital_USAA_2026-07-03.png", "image/20260703/report_cathay_EA_2026-07-03.png", "image/20260703/report_uni_49YTW_2026-07-03.png", "image/20260703/report_uni_61YTW_2026-07-03.png", "image/20260703/report_uni_63YTW_2026-07-03.png"];
 const reportDate = '20260703';
 const indexImages = [];
 const indexData = {"加權指數": {"symbol": "tse_t00.tw", "date": "2026-07-03", "close": 46780.62, "change": 36.46, "pct": 0.08}, "櫃買指數": {"symbol": "otc_o00.tw", "date": "2026-07-03", "close": 445.38, "change": 5.87, "pct": 1.34}};
