f5ac3e83e9a655e440b92cdbb11851b633a5f0c4
YuShiTing <shiy03313@gmail.com>
2026-07-03 18:42:18 +0800

    Update: 2026-07-03 18:42:17

--- images.js
+++ images.js
@@ -1,4 +1,4 @@
 const imgList = [];
 const reportDate = '20260703';
-const indexImages = ["image/20260703/TWII_20260703.png", "image/20260703/TWOII_20260703.png"];
+const indexImages = [];
 const indexData = {};
