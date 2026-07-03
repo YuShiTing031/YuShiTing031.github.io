96efff5fda410ca9b9bb8f7c8573c53f05f74584
YuShiTing <shiy03313@gmail.com>
2026-07-03 19:05:06 +0800

    Update: 2026-07-03 19:05:06

--- images.js
+++ images.js
@@ -1,4 +1,4 @@
 const imgList = [];
 const reportDate = '20260703';
 const indexImages = [];
-const indexData = {};
+const indexData = {"加權指數": {"symbol": "tse_t00.tw", "date": "2026-07-03", "close": 46780.62, "change": 36.46, "pct": 0.08}, "櫃買指數": {"symbol": "otc_o00.tw", "date": "2026-07-03", "close": 445.38, "change": 5.87, "pct": 1.34}};
